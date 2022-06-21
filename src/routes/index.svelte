f18cf7f76c50bfa5b29f012999acdd987050cb7a
Matti Katila <matti.katila@gmail.com>
2022-06-21 03:26:32 +0300

    First map like rendering of forests

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,10 +2,6 @@
 	export const prerender = true;
 </script>
 
-<script>
-	import Counter from '$lib/Counter.svelte';
-</script>
-
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
@@ -27,7 +23,6 @@
 		try editing <strong>src/routes/index.svelte</strong>
 	</h2>
 
-	<Counter />
 </section>
 
 <style>
@@ -58,4 +53,481 @@
 		top: 0;
 		display: block;
 	}
+
+	#mapid {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+    }
+
 </style>
+
+<div id="mapid"></div>
+
+<script>
+import { onMount } from 'svelte'
+import { booleanPointInPolygon, point, lineString, bbox, polygon } from "@turf/turf"
+
+let map;
+
+
+const loadRoutes = async () => {
+    const mapData = await fetch(`./routes.json`, {
+            method: 'GET',
+    })
+	const routes = await mapData.json()
+	for (const route of routes) {
+        new L.GPX(route.file, {
+            async: true,
+            gpx_options: {
+                parseElements: ['track', 'route']
+            }
+        }).on('loaded', function(e) {
+			//map.fitBounds(e.target.getBounds());
+		}).addTo(map);
+	}
+    console.log(routes)
+}
+
+onMount(async () => {
+	someStuff()
+
+	map = L.map('mapid').setView([65.330, 26.818], 176);
+	
+    L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+	L.tileLayer('https://tiles.kartat.kapsi.fi/peruskartta/{z}/{x}/{y}.jpg', {
+        attribution: '&copy; MML'
+	}).addTo(map);
+
+    L.tileLayer('//tile.waymarkedtrails.org/hiking/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://waymarkedtrails.org">Waymarked Trails</a>'
+    }).addTo(map);
+/*	
+	const wfstPointOptions = {
+		crs: L.CRS.EPSG4326,
+		showExisting: true,
+		geometryField: 'Shape',
+		url: 'https://rajapinnat.metsaan.fi/geoserver/Avoinmetsatieto/stand/ows',
+		typeNS: 'avoinmetsatieto',
+		typeName: 'stand',
+		maxFeatures: 90,
+		opacity: 1,
+		style: function(layer) {
+			// you can use if statemt etc
+			return {
+			color: 'black',
+			weight: 1
+			}
+		},
+	};
+	const wfstPoint = new L.WFST(wfstPointOptions, new L.Format.GeoJSON({
+		crs: L.CRS.EPSG4326,
+		pointToLayer(geoJsonPoint, latlng) {
+			const layer = new L.CircleMarker(latlng, {
+			radius: 10,
+			});
+			return layer;
+		},
+	}));
+	wfstPoint.addTo(map).on('load', function () {
+      //map.fitBounds(wfstPoint);
+	});
+*/
+		// https://rajapinnat.metsaan.fi/geoserver/Avoinmetsatieto/stand/wfs?
+		// SERVICE=WFS&REQUEST=GetFeature&
+		// VERSION=2.0.0&
+		// TYPENAMES=Avoinmetsatieto:stand&
+		// STARTINDEX=0&
+		// COUNT=10000&
+		// SRSNAME=urn:ogc:def:crs:EPSG::3067&
+		// BBOX=432080.04426494485232979,6896253.27520507760345936,435238.8645905137527734,6899056.8352501280605793,urn:ogc:def:crs:EPSG::3067
+
+/*
+*/	
+	const id2obj = {}
+	const trees = []
+	let active = null
+
+	const renderSoil = (ctx, main, sub, soil, act) => {
+		//console.log(main, sub, soil)
+		ctx.fillStyle = 'magenta'
+		switch (main) {
+		case 1: ctx.fillStyle = 'LimeGreen'; // Metsämaa
+			switch (sub) {
+			case 1: ctx.fillStyle = 'LimeGreen'; break;  // Kangas
+			case 2: ctx.fillStyle = 'MediumSeaGreen'; break;  // Korpi
+			case 3: ctx.fillStyle = 'DarkSeaGreen'; break;  // Räme
+			case 4: ctx.fillStyle = 'YellowGreen'; break;  // Neva
+			case 5: ctx.fillStyle = 'DarkKhaki'; break;  // Letto				
+			}
+			break;
+		case 2: ctx.fillStyle = 'orange'; break; // Kitumaa
+		case 3: ctx.fillStyle = 'brown'; break; // Joutomaa
+		case 4: break; // Muu metsätalousmaa
+		case 5: break; // Tontti
+		case 6: ctx.fillStyle = 'yellow'; break; // Maatalousmaa
+		case 7: break; // Muu maa
+		case 8: ctx.fillStyle = 'blue'; break; // Vesistö
+		}
+		if (act) ctx.fillStyle = 'yellow'
+		ctx.fill()
+	}
+
+	const scaleX = 0.016, scaleY = 0.016
+	const renderTrees = (ctx, params) => {
+		console.log('render trees', trees.length)
+		const b = params.bounds
+		const dx = (b.getEast() - b.getWest())
+		const dy = (b.getNorth() - b.getSouth())
+		//console.log(b.getEast() - b.getWest(), dx, dy, params.canvas.width)
+
+		for (const tree of trees) {
+			const pt = tree.pt
+			const x = (pt[0]-b.getWest())/dx * params.canvas.width
+			const y = params.canvas.height - (pt[1]-b.getSouth())/dy * params.canvas.height
+
+
+			ctx.beginPath()
+			ctx.moveTo(x-Math.max(tree.height/7, 1), y)
+		    ctx.lineTo(x, y-Math.max(tree.height, 3))
+    		ctx.lineTo(x+Math.max(tree.height/7, 1), y)
+			ctx.closePath()
+
+			ctx.fillStyle = 'brown'
+			ctx.fill()
+
+			switch (tree.specie) {
+			case 1: //Mänty
+
+			ctx.beginPath()
+			ctx.arc(x, y-tree.height*0.7, tree.height/2, Math.PI*0.8, Math.PI*0.2)
+			ctx.closePath()
+
+			ctx.fillStyle = 'ForestGreen'
+			ctx.fill()
+			
+				break; 
+			case 2: //Kuusi 
+			ctx.beginPath()
+			ctx.moveTo(x-tree.height/3, y-2)
+		    ctx.lineTo(x, y-tree.height)
+    		ctx.lineTo(x+tree.height/3, y-2)
+			ctx.closePath()
+
+			ctx.fillStyle = 'ForestGreen'
+			ctx.fill()
+			
+				break; 
+			case 3: //Rauduskoivu
+			case 4: //Hieskoivu
+			ctx.fillStyle = 'LightYellow'
+			ctx.fill()
+
+			ctx.beginPath()
+			ctx.arc(x, y-tree.height*1, tree.height/3, Math.PI*0.85, Math.PI*0.15)
+			ctx.arc(x, y-tree.height*0.6, tree.height/2.5, Math.PI*0.85, Math.PI*0.15)
+			ctx.closePath()
+
+			ctx.fillStyle = 'GreenYellow'
+			ctx.fill()
+
+				break;
+			case 5: //Haapa
+			ctx.fillStyle = 'DarkGray'
+			ctx.fill()
+
+			ctx.beginPath()
+			ctx.arc(x, y-tree.height*1, tree.height/3, Math.PI*0.85, Math.PI*0.15)
+			ctx.arc(x, y-tree.height*0.6, tree.height/2.5, Math.PI*0.85, Math.PI*0.15)
+			ctx.closePath()
+
+			ctx.fillStyle = 'DarkGreen'
+			ctx.fill()
+				break;
+			case 6: //Harmaaleppä
+			case 7: //Tervaleppä
+			case 8: //Muu havupuu
+			case 9: //Muu lehtipuu
+			case 10: //Douglaskuusi
+			case 11: //Kataja
+			case 12: //Kontortamänty
+			case 13: //Kynäjalava
+			case 14: //Lehtikuusi
+			case 15: //Metsälehmus
+			case 16: //Mustakuusi
+			case 17: //Paju
+			case 18: //Pihlaja
+			case 19: //Pihta
+			case 20: //Raita
+			case 21: //Saarni
+			case 22: //Sembramänty
+			case 23: //Serbiankuusi
+			case 24: //Tammi
+			case 25: //Tuomi
+			case 26: //Vaahtera
+			case 27: //Visakoivu
+			case 28: //Vuorijalava
+			case 29: //Lehtipuu
+			case 30: //Havupuu
+			default:
+				console.log('missing render tree render for ', tree.specie)
+			}
+		}
+
+		// tiheys voidaan laskea pinta-ala/runkoluku
+
+	}
+
+	const drawOnCanvas = (canvasOverlay, params) => {
+		const b = params.bounds
+		bbFetch(params.bounds)
+
+		const ctx = params.canvas.getContext('2d');
+        ctx.clearRect(0, 0, params.canvas.width, params.canvas.height);
+
+		const dx = (b.getEast() - b.getWest())
+		const dy = (b.getNorth() - b.getSouth())
+
+		for (const f of Object.values(id2obj)) {
+
+			ctx.beginPath()
+			for (let i=0; i<f.geometry.coordinates[0].length; i++) {
+				const pt = f.geometry.coordinates[0][i]
+				const x = (pt[0]-b.getWest())/dx * params.canvas.width
+				const y = params.canvas.height - (pt[1]-b.getSouth())/dy * params.canvas.height
+				i==0 ? ctx.moveTo(x,y): ctx.lineTo(x,y)
+			}
+			ctx.closePath()
+
+			const p = f.properties
+			renderSoil(ctx, p.MAINGROUP, p.SUBGROUP, p.SOILTYPE, f==active)
+
+
+			ctx.lineWidth = 0.3;
+			ctx.strokeStyle = 'black';
+			ctx.stroke()
+			//console.log(f)
+		}
+		renderTrees(ctx, params)
+	}
+	const addTrees = f => {
+		console.log('add trees')
+		const p = f.properties
+		const geom = polygon(f.geometry.coordinates, f.properties)
+		//renderForest(ctx, geom, p.AREA, p.STEMCOUNT, p.MAINTREESPECIES, p.MEANAGE, p.MEANDIAMETER, p.MEANHEIGHT)
+	
+		const freq = p.STEMCOUNT > 0 ? Math.sqrt(p.AREA / p.STEMCOUNT): 10000
+		f.freq = freq
+		const bb = bbox(geom)
+		let pt = { x: bb[1], y: bb[2]} // west, north
+		while (pt.y > bb[0]) {
+			const P = point([pt.y, pt.x])
+			if (booleanPointInPolygon(P, geom)) {
+				trees.push({
+					pt: P.geometry.coordinates,
+					height: p.MEANHEIGHT,
+					specie: p.MAINTREESPECIES
+				})
+			}
+			pt.x += freq * scaleX
+			if (pt.x > bb[3]) {
+				pt.y -= freq * scaleY
+				pt.x -= (bb[3]-[bb[1]])
+			}
+		}
+
+
+	}
+
+	const bbFetch = (bounds) => {
+		fetch(`https://rajapinnat.metsaan.fi/geoserver/Avoinmetsatieto/stand/wfs?SERVICE=WFS&REQUEST=GetFeature&VERSION=2.0.0&TYPENAMES=Avoinmetsatieto:stand&STARTINDEX=0&COUNT=10000&SRSNAME=urn:ogc:def:crs:EPSG::4326&BBOX=${bounds.getSouth()},${bounds.getWest()},${bounds.getNorth()},${bounds.getEast()},urn:ogc:def:crs:EPSG::4326&outputFormat=json`)
+			.then(data => {
+				//console.log('data', data.json())
+				data.json().then(d => {
+					let chg = false
+					for (const f of d.features) {
+						if (!chg && trees.length < 1000)
+						if (id2obj[f.id] === undefined) {
+							chg = true
+							id2obj[f.id] = f
+							addTrees(f)
+						}
+					}
+					console.log(d)
+					if (chg) overlay.redraw()
+				})
+			})
+	}
+	const overlay = L.canvasOverlay()
+	overlay.drawing(drawOnCanvas)
+		.addTo(map)
+
+	overlay.addEventListener('click', ev => {
+		const latLng = map.layerPointToLatLng(
+			L.point(ev.offsetX, ev.offsetY))
+
+		for (const f of Object.values(id2obj)) {
+			const geom = polygon(f.geometry.coordinates, f.properties)
+			const P = point([latLng.lng, latLng.lat])
+			if (booleanPointInPolygon(P, geom)) {
+				active = active==f? null: f
+				console.log(f)
+				overlay.redraw()
+			}
+		}
+	})
+
+
+    const jkl = [62.24452234218559, 25.748742842059354];
+    const circle = L.circle(jkl, {
+        radius: 150*1000, fill: false
+    })
+	circle.addTo(map);
+	map.setView(L.latLng(62.20999108080592, 25.68709194660187), 17)
+    //map.fitBounds(circle.getBounds());
+
+})
+
+
+
+function someStuff() {
+
+/*
+ Generic  Canvas Overlay for leaflet, 
+ Stanislav Sumbera, April , 2014
+ - added userDrawFunc that is called when Canvas need to be redrawn
+ - added few useful params fro userDrawFunc callback
+  - fixed resize map bug
+  inspired & portions taken from  :   https://github.com/Leaflet/Leaflet.heat
+  
+*/
+L.CanvasOverlay = L.Layer.extend({
+
+    initialize: function (userDrawFunc, options) {
+        this._userDrawFunc = userDrawFunc;
+        L.setOptions(this, options);
+    },
+
+    drawing: function (userDrawFunc) {
+        this._userDrawFunc = userDrawFunc;
+        return this;
+    },
+
+    params:function(options){
+        L.setOptions(this, options);
+        return this;
+    },
+    
+    canvas: function () {
+        return this._canvas;
+    },
+
+    redraw: function () {
+        if (!this._frame) {
+            this._frame = L.Util.requestAnimFrame(this._redraw, this);
+        }
+        return this;
+    },
+
+    
+  
+    onAdd: function (map) {
+        this._map = map;
+        this._canvas = L.DomUtil.create('canvas', 'leaflet-heatmap-layer');
+
+        var size = this._map.getSize();
+        this._canvas.width = size.x;
+        this._canvas.height = size.y;
+
+        var animated = this._map.options.zoomAnimation && L.Browser.any3d;
+        L.DomUtil.addClass(this._canvas, 'leaflet-zoom-' + (animated ? 'animated' : 'hide'));
+
+
+        map._panes.overlayPane.appendChild(this._canvas);
+
+        map.on('moveend', this._reset, this);
+        map.on('resize',  this._resize, this);
+
+        if (map.options.zoomAnimation && L.Browser.any3d) {
+            map.on('zoomanim', this._animateZoom, this);
+        }
+
+        this._reset();
+    },
+
+    onRemove: function (map) {
+        map.getPanes().overlayPane.removeChild(this._canvas);
+ 
+        map.off('moveend', this._reset, this);
+        map.off('resize', this._resize, this);
+
+        if (map.options.zoomAnimation) {
+            map.off('zoomanim', this._animateZoom, this);
+        }
+        this_canvas = null;
+
+    },
+
+    addTo: function (map) {
+        map.addLayer(this);
+        return this;
+	},
+	
+	addEventListener: function(ev, func) {
+		this._canvas.addEventListener(ev, func, false)
+	},
+
+    _resize: function (resizeEvent) {
+        this._canvas.width  = resizeEvent.newSize.x;
+        this._canvas.height = resizeEvent.newSize.y;
+    },
+    _reset: function () {
+        var topLeft = this._map.containerPointToLayerPoint([0, 0]);
+        L.DomUtil.setPosition(this._canvas, topLeft);
+        this._redraw();
+    },
+
+    _redraw: function () {
+		console.log('redraw')
+        var size     = this._map.getSize();
+        var bounds   = this._map.getBounds();
+        var zoomScale = (size.x * 180) / (20037508.34  * (bounds.getEast() - bounds.getWest())); // resolution = 1/zoomScale
+        var zoom = this._map.getZoom();
+     
+        // console.time('process');
+
+        if (this._userDrawFunc) {
+            this._userDrawFunc(this,
+                                {
+                                    canvas   :this._canvas,
+                                    bounds   : bounds,
+                                    size     : size,
+                                    zoomScale: zoomScale,
+                                    zoom : zoom,
+                                    options: this.options
+                               });
+        }
+       
+       
+        // console.timeEnd('process');
+        
+        this._frame = null;
+    },
+
+    _animateZoom: function (e) {
+        var scale = this._map.getZoomScale(e.zoom),
+            offset = this._map._getCenterOffset(e.center)._multiplyBy(-scale).subtract(this._map._getMapPanePos());
+
+        //this._canvas.style[L.DomUtil.TRANSFORM] = L.DomUtil.getTranslateString(offset) + ' scale(' + scale + ')';
+
+    }
+});
+
+L.canvasOverlay = function (userDrawFunc, options) {
+    return new L.CanvasOverlay(userDrawFunc, options);
+};
+}
+
+</script>
